b0b39dc4b0448ca55e52baf0cd5467844269d110
Michael Bey <michael.bey@stu.bmcc.cuny.edu>
2019-04-03 23:13:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
 window.addEventListener("load", () => {
+  
   //start your code here
+  
+  const lowerCase = "abcdefghijklmnopqrstuvwxyz"
+  const upperCase = "QWERTYUIOPASDFGHJKLZXCVBNM"
+  const specialSymbol ='!@#$%^&*()'
+  const numbers = '1234567890'
+  
+  function onSubmit(event){
+    event.preventDefault()
+    const input = document.getElementById("passwordLength")
+    const howMany = input.value
+    let startingString=''
+    
+    
+    
+    for (let i=0; i <howMany; i++){
+      
+    if (startingString.length < howMany){
+    let randomIndex = Math.floor(Math.random() * lowerCase.length)
+    startingString += lowerCase[randomIndex]
+  }
+  
+     if (startingString.length < howMany){
+  randomIndex = Math.floor(Math.random() * upperCase.length)
+    startingString += upperCase[randomIndex]
+  }
+  
+
+    if (startingString.length < howMany){
+    randomIndex = Math.floor(Math.random() * numbers.length)
+    startingString += numbers[randomIndex]
+  }
+  
+  if (startingString.length < howMany){
+    randomIndex = Math.floor(Math.random() * specialSymbol.length)
+    startingString += specialSymbol[randomIndex]
+  }
+    }
+  console.log(startingString)
+  const output = document.getElementById('result')
+  output.innerHTML = startingString
+  }
+  document.addEventListener('submit', onSubmit)
 });
